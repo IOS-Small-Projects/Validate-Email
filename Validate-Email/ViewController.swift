3a2909e521cd83d0af954bf74e87f6e2758c139b
Rohit Kanade <kanade3@gmail.com>
2016-09-23 19:53:32 +0530

    no message

--- Validate-Email/ViewController.swift
+++ Validate-Email/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
+extension String
+{
+    func isEmail() -> Bool
+    {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluateWithObject(self)
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var email: UITextField!
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func isValidate(sender: AnyObject) {
+        if  email.text!.isEmail() == true{
+            label.text = email.text! + " is email"
+        }else{
+            label.text = email.text! + " is not email"
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
